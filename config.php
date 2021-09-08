ebc03d1733899b3650d0a32f485eb16efa8919f9
khonghung <ahihimaianh3@gmail.com>
2021-09-08 12:34:15 +0700

    code

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "12345678@");
+define("DB", "mvc0");
+define("ROOT", dirname(__FILE__));
+define("BASE_URL", "http://localhost/");
+
